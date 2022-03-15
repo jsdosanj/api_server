c7fd636c31b402d0c5f9c5d050405e46b8d60032
Jasvant Singh Dosanjh <jasvantdosanjh@outlook.com>
2022-03-15 10:22:08 -0700

    Add files via upload

--- api_server_config.sh
+++ api_server_config.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 # Introduction
-echo "Nice to meet you Administrator :) "
-HOSTNAME="i13.api.omnifocus.com"
+echo "Nice to meet you Administrator :) I see you are setting up another API Server! That's nice! Let's get started, shall we! "
+echo "What would you like to name this api server? Our previous api servers use the naming scheme i##.api.omnifocus.com"
+read varname
+HOSTNAME=varname".api.omnifocus.com"
 TIMEZONE="America/Seattle" # 'systemsetup -listtimezones'
-echo "Please manually install the 'dpkg' package to ensure that the script can install the other packages :) "
 
 # Ask for administrator password to run script
 echo " Ask for the administrator password for the duration of this script"
@@ -127,24 +128,28 @@ sudo pmset -a autorestart 1
 # Restart automatically if the computer freezes
 sudo systemsetup -setrestartfreeze on
 
-# Remove the sleep image file to save disk space
-#sudo rm /private/var/vm/sleepimage
-
 ###############################################################################
 # Install Packages                                                            #
 ###############################################################################
 
-#Install Packages
-#dpkg --install acmetool-0.0.67_1.pkg
-#dpkg --install filebeat-8.0.0.pkg
-#dpkg --install grep-3.7.pkg
-#dpkg --install kafka-3.1.0.pkg
-#dpkg --install metricbeat-8.0.0.pkg
-#dpkg --install monitoring-plugins-2.3.1.pkg #nagios plugins: https://www.monitoring-plugins.org/gi
-#dpkg --install nginx-1.21.6.pkg
-#dpkg --install socat-1.7.4.3.pkg
-#dpkg --install zookeeper-3.7.0_1.pkg
-#dpkg --install certbot-1.23.0.pkg
+echo "Installing nginx"
+sudo installer -pkg /Users/administrator/Desktop/nginx-1.21.6.pkg -target /
+
+echo "Installing socat"
+sudo installer -pkg /Users/administrator/Desktop/socat-1.7.4.3.pkg -target /
+
+echo "Installing acme.sh"
+systemctl reload nginx.service
+curl https://get.acme.sh | sh -s email=rangers@omnigroup.com
+echo "Please configure nginx so that Acme.sh can pull the proper certificates into nginx"
+
+echo "Installing metricbeat"
+curl -L -O https://artifacts.elastic.co/downloads/beats/metricbeat/metricbeat-8.1.0-darwin-x86_64.tar.gz
+tar xzvf metricbeat-8.1.0-darwin-x86_64.tar.gz
+
+echo " Installing filebeat"
+curl -L -O https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-8.1.0-darwin-x86_64.tar.gz
+tar xzvf filebeat-8.1.0-darwin-x86_64.tar.gz
 
 ###############################################################################
 # SSH, Firewall and Networking                                                #
@@ -166,19 +171,34 @@ ssh-keygen -t rsa
 # To view public-ssh key
 cat ~/.ssh/id_rsa.pub
 
-# Install Acme.sh
-# https://www.rmedgar.com/blog/using-acme-sh-with-nginx/
-# https://www.cyberciti.biz/faq/how-to-configure-nginx-with-free-lets-encrypt-ssl-certificate-on-debian-or-ubuntu-linux/
-systemctl reload nginx.service
-curl https://get.acme.sh | sh -s email=rangers@omnigroup.com
-echo "Please configure nginx so that Acme.sh can pull the proper certificates into nginx"
+# Setup acme.sh
+# Set the notification level:  Default value is 2.
+#0: disabled, no notification will be sent.
+#1: send notification only when there is an error. No news is good news.
+#2: send notification when a cert is successfully renewed, or there is an error
+#3: send notification when a cert is skipped, renewed, or error. You will receive notification every night with this level.
+acme.sh --set-notify  [--notify-hook mail]
+acme.sh --set-notify  [--notify-level 2]
+acme.sh --set-notify  [--notify-mode 0]
+export MAIL_FROM="rangers@omnigroup.com" # or "Xxx Xxx <xxx@xxx.com>", currently works only with sendmail
+export MAIL_TO="rangers@omnigroup.com"   # your account e-mail will be used as default if available
+acme.sh --issue --standalone -d i13.api.omnifocus.com
+
+# Look for suspicious and self-signed SSL certificates
+tcpdump -s 1500 -A '(tcp[((tcp[12:1] & 0xf0) >> 2)+5:1] = 0x01) and (tcp[((12:1] & 0xf0) >> 2) :1] = 0x16)'
+
+# Use netcat to listen for scanning threats
+nc -v -k -l 80
+nc -v -k -l 443
+nmap -p 80-443 192.168.0.1
 
 # Show IP address, hostname, OS version when clicking the clock in the login window
 #echo " Show IP address, hostname, OS version when clicking the clock in the login window"
-#sudo defaults write /Library/Preferences/com.apple.loginwindow AdminHostInfo HostName
+sudo defaults write /Library/Preferences/com.apple.loginwindow AdminHostInfo HostName
 
 # Get Server IP Address; network config for ethO
 ip="$(ifconfig | grep -A 1 'eth0' | tail -1 | cut -d ':' -f 2 | cut -d ' ' -f 1)"
+echo `ifconfig eth0 2>/dev/null|awk '/inet addr:/ {print $2}'|sed 's/addr://'`
 
 #firewall-cmd  --permanent --add-port 8000/tcp     # plain-HTTP version of service
 #firewall-cmd  --permanent --add-port 2022/tcp     # encrypted debug/inspection manhole
@@ -191,6 +211,16 @@ log stream --info | /usr/local/bin/socat -dddd STDIN TCP4:search1.sync.omnigroup
 # Start pf
 pfctl -e -f /etc/pf.conf
 
+# View system info
+uname -a
+uptime
+timedatectl
+mount
+echo $PATH
+cat /root/.ssh/authorized_keys
+ps -aux
+
 # Reboot the system
+echo "Well that was fun, wasn't it? I hope everything worked as it should :) "
 echo "Done. Note that some of these changes require a logout/restart to take effect."
 exec "$SHELL"
